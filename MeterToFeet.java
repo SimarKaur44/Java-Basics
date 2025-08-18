8b13ba190b1590b8d6dc20afbaf383ff4d253c14
Simar <simarpreet.kaur.2044@gmail.com>
2025-08-18 18:14:06 +0530

    Meter to feet

---
+++ MeterToFeet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class MeterToFeet{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to convert");
+        double a=sc.nextDouble();
+        a=a*3.37;
+        System.out.println("Feet = "+a);
+            
+        
+    }
+}
