f8dbc7bff8b427a7344bed0a2245ba9dd5940f23
Simar <simarpreet.kaur.2044@gmail.com>
2025-08-18 17:23:03 +0530

    Numer pattern

---
+++ NumberPattern.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class NumberPattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number:");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number:");
+        int b = input.nextInt();
+
+        System.out.print("Enter third number:");
+        int c = input.nextInt();
+
+        if(a < b && b < c) {
+            System.out.println("Strictly Increasing");
+        } 
+        else if(a > b && b > c) {
+            System.out.println("Strictly Decreasing");
+        } 
+        else if(b > a && b > c) {
+            System.out.println("Middle number is largest");
+        } 
+        else {
+            System.out.println("No specific pattern");
+        }
+    }                                                       
+}
