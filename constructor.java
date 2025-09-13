5111a5de937f97005040786e49578b1a6990adf2
SimarKaur44 <simarpreet.kaur.2044@gmail.com>
2025-09-13 11:26:35 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,9 @@
+class constructor{
+   constructor(){
+     super();
+     System.out.println("Constructor Called");
+   }
+  public static void main(String[] args){
+    constructor geek=nem constructor();
+  }
+}
